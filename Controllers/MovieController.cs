9ecaa405a69fd348ccd942343fe50ad54c9785bc
Srikar Naidu <n.srikar22@gmail.com>
2023-03-25 19:54:19 +0530

    feat(app): configuring project

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManager.Models;
+
+namespace MovieManager.Controllers
+{
+    public class MovieController : Controller
+    {
+        public Movie GetMovies(string genre)
+        {
+            return new Movie();
+        }
+    }
+}
