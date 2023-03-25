a7841a83538797dc7b5861c47a483620df1743f2
Srikar Naidu <n.srikar22@gmail.com>
2023-03-25 21:10:57 +0530

    feat(app): building code workflow

---
+++ Models/MovieModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieManager.Models
+{
+    public class MovieModel
+    {
+        private readonly AppDbContext context;
+
+        public MovieModel(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Movie> getMovies(string genre)
+        {
+            return context.Movies.Where(movie => movie.Genre == genre).ToList();
+        }
+    }
+}
