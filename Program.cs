1863ea25889de2f2647fbc58c190ae0c7668c662
Srikar Naidu <n.srikar22@gmail.com>
2023-03-25 19:54:19 +0530

    feat(app): configuring project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using MovieManager.Models;
+
 namespace MovieManager
 {
     public class Program
@@ -6,15 +10,30 @@ namespace MovieManager
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
+            var env = app.Environment;
+            var config = app.Configuration;
 
+            builder.Services.AddMvc();
             builder.Services.AddSwaggerDocument();
+            builder.Services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(config.GetConnectionString("MovieDBConnectionString")));
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home/{action=Index}/{id?}");
+            });
 
             app.UseOpenApi();
             app.UseSwaggerUi3();
 
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", () => "Hello World!");
 
-            app.Run();
+            //app.Run();
         }
     }
 }
